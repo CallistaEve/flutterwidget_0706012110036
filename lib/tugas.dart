c2ca660aaa339db206253c5eb1a3cb88a6bfe375
CallistaEve <eyaurentius@gmail.com>
2023-10-10 22:33:00 +0700

    Done

--- lib/tugas.dart
+++ lib/tugas.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TugasPage extends StatefulWidget {
-  const TugasPage({super.key});
+  const TugasPage({Key? key}) : super(key: key);
 
   @override
   State<TugasPage> createState() => _TugasPageState();
@@ -17,39 +17,49 @@ class _TugasPageState extends State<TugasPage> {
         backgroundColor: Colors.blue[400],
         foregroundColor: Colors.white,
       ),
-      floatingActionButton: FloatingActionButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-        foregroundColor: isPressed ? Colors.red : Colors.grey,
-        onPressed: () => setState(() => isPressed = !isPressed),
-        backgroundColor: Colors.white,
-        child: const Icon(Icons.heart_broken_sharp),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
       body: Container(
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Colors.yellow.shade200,
-            Colors.blue,
-          ],
-        )),
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.yellow.shade200,
+              Colors.blue,
+            ],
+          ),
+        ),
         child: Column(
           children: [
             Flexible(
               flex: 2,
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                child: ClipRRect(
-                  child: Image.asset(
-                    'images/gambar1.webp',
-                    fit: BoxFit.fill,
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: ClipRRect(
+                      child: Image.asset(
+                        'images/gambar1.webp',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: 10, 
+                    right: 10, 
+                    child: FloatingActionButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100)),
+                      foregroundColor: isPressed ? Colors.red : Colors.grey,
+                      onPressed: () =>
+                          setState(() => isPressed = !isPressed),
+                      backgroundColor: Colors.white,
+                      child: const Icon(Icons.favorite),
+                    ),
+                  ),
+                ],
               ),
             ),
             Flexible(
@@ -133,36 +143,40 @@ class _TugasPageState extends State<TugasPage> {
                 ),
               ),
             ),
-            Container(
-               alignment: Alignment.center,
-              width: double.infinity,
-              child: Row(
-                children: [
-                          Text(
-                            "Royal Kuningan Hotel",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.bold), 
-                                
-                          ),
-                        ],
-                      ),
-            ),
             Flexible(
               flex: 4,
               child: Container(
-                margin: EdgeInsets.all(5),
+                margin: EdgeInsets.only(top:10, left: 5, right: 5),
                 width: double.infinity,
                 height: double.infinity,
-                child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Column(children: [
-                        Text(
-                            "Id leo in vitae turpis. Tortor at auctor urna nunc id cursus. Tristique senectus et netus et. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Pharetra sit amet aliquam id diam. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Viverra ipsum nunc aliquet bibendum. Purus sit amet luctus venenatis lectus magna. Amet volutpat consequat mauris nunc congue nisi vitae. Netus et malesuada fames ac turpis egestas. Arcu vitae elementum curabitur vitae nunc sed. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Neque convallis a cras semper auctor neque vitae tempus. Aliquam nulla facilisi cras fermentum odio. Sollicitudin tempor id eu nisl nunc mi ipsum. Id leo in vitae turpis. Tortor at auctor urna nunc id cursus. Tristique senectus et netus et. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Pharetra sit amet aliquam id diam. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Viverra ipsum nunc aliquet bibendum. Purus sit amet luctus venenatis lectus magna. Amet volutpat consequat mauris nunc congue nisi vitae. Netus et malesuada fames ac turpis egestas. Arcu vitae elementum curabitur vitae nunc sed. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Neque convallis a cras semper auctor neque vitae tempus. Aliquam nulla facilisi cras fermentum odio. Sollicitudin tempor id eu nisl nunc mi ipsum.")
-                      ]),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        "Welcome To Royal Kuningan Hotel",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
+                    Expanded(
+                      child: ListView(
+                        scrollDirection: Axis.vertical,
+                        children: [
+                          Text(
+                              "The Royal Kuningan Hotel Jakarta features 369 beautifully appointed guest rooms, each including internet access, working desk, daily newspaper, smoking & non smoking rooms, LCD plasma screen TV. The hotel offers wonderful recreational facilities such as spa, outdoor pool, fitness center, massage, sauna & salon to make your stay truly unforgettable. Superb facilities and an excellent location make the Royal Kuningan Hotel Jakarta the perfect base from which to enjoy your stay in Jakarta. \n\nBuilt in 2012, Royal Kuningan Hotel is a distinct addition to the city’s skyline. Only 10 minutes from the excitement of the city, this 4 (four) Stars hotel commands an excellent location and provides access to the city’s biggest attractions. For those of you who want to venture out, National Monument, Ragunan Zoo, Taman Ismail Marzuki Art Center, and various choices of Shopping Town are just some of the attractions available to visitors. Royal Kuningan Hotel Jakarta offers impeccable service and all the essential amenities to invigorate travelers.",style: TextStyle(
+                          fontSize: 17,
+                        ),
+                        )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              )
+              ),
+            ),
           ],
         ),
       ),
